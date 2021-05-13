c28958213df98c5404cdd8e053db9cdf4652c1dd
Evgeniy <ghenya.t@gmail.com>
2021-05-13 08:04:14 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,101 @@
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+// const date = require(__dirname + "/date.js");
+const port = 3000;
+// let items = ["by food"];
+let workItems = [];
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+
+mongoose
+  .connect("mongodb://localhost:27017/todoListDB", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then((res) => console.log("mongoose connected"));
+
+const itemsSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Name required"],
+  },
+});
+const Item = mongoose.model("Item", itemsSchema);
+
+const item1 = new Item({
+  name: "By food",
+});
+
+const item2 = new Item({
+  name: "Make breakfast",
+});
+
+const item3 = new Item({
+  name: "Eat breakfast",
+});
+const defaultItems = [item1, item2, item3];
+let todos = [];
+
+app.get("/", (req, res) => {
+  Item.find({}, (err, todos) => {
+    if (!err) {
+      console.log("No errors");
+      if (todos.length === 0) {
+        console.log("No Items");
+        Item.insertMany(defaultItems, (err) => {
+          if (!err) {
+            console.log("Default items succesfully added");
+          } else {
+            console.log(err);
+          }
+        });
+        res.redirect("/");
+      } else {
+        res.render("list", { listTitle: "Today", newListItems: todos });
+      }
+    } else {
+      console.log(err);
+    }
+  });
+});
+
+app.post("/", (req, res) => {
+  const nextItem = new Item({
+    name: req.body.newItem,
+  });
+
+  nextItem.save();
+  res.redirect("/");
+  // console.log(req.body);
+
+  // if (req.body.list === "Work") {
+  //   Item.insertOne(nextItem, (err) => {
+
+  //   });
+  //   res.redirect("/work");
+  // } else {
+  //   Item.insertOne(nextItem);
+  //   res.redirect("/");
+  // }
+});
+
+app.post("/delete", (req, res) => {
+  const idToDelete = req.body.deleteCheckbox;
+  console.log("idToDelete  " + idToDelete);
+  Item.findByIdAndRemove(idToDelete, (err) => {
+    console.log(err);
+  });
+});
+
+app.get("/work", (req, res) => {
+  res.render("list", { listTitle: "Work list", newListItems: workItems });
+});
+
+app.get("/about", (req, res) => {
+  res.render("about");
+});
+app.listen(port, () => {
+  console.log("Server started on port " + port);
+});
